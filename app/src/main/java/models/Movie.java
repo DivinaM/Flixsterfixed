1eb9095346aebf9803a2472eb9d167813f2387f6
DivinaM <divina.mcgowan@gmail.com>
2020-09-14 17:21:46 -0500

    commit pt two

--- app/src/main/java/models/Movie.java
+++ app/src/main/java/models/Movie.java
@@ -6,21 +6,31 @@ import android.util.JsonReader;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
+@Parcel
 
 public class Movie {
+    int movieId;
     String backdropPath;
     String posterPath;
     String title;
     String overview;
+    double rating;
+
+    public Movie() {}
+
+
 
     public Movie (JSONObject jsonObject) throws JSONException {
         backdropPath = jsonObject.getString("backdrop_path");
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
+        rating = jsonObject.getDouble("vote_avergae");
+        movieId = jsonObject.getInt("movieId");
 
 
 
@@ -46,9 +56,21 @@ public class Movie {
 
     public String getTitle() {
         return title;
+
     }
 
     public String getOverview() {
         return overview;
+
+    }
+
+    public int getMovieId() {
+        return movieId;
+    }
+
+    public double getRating() {
+        return rating;
+
+
     }
 }
